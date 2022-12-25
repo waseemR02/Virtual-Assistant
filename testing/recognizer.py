e8fcc6ff08476604be488c0666c46490e4b8048e
waseemR02 <waseem.riaz.9999@gmail.com>
2022-12-25 13:38:54 +0530

    testing packages for getting an idea on them Packages: Speech Recogntion

---
+++ testing/recognizer.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+recording = sr.Recognizer()
+
+with sr.Microphone(device_index=0) as source: 
+	recording.adjust_for_ambient_noise(source)
+	print("Please Say something:")
+	audio = recording.listen(source)
+
+try:
+   print("You said: \n" + recording.recognize_google(audio))
+except Exception as e:
+   print(e)
