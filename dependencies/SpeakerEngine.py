4981398059765b4733e876c12b9e34e827cf8559
waseemR02 <waseem.riaz.9999@gmail.com>
2022-12-26 00:18:32 +0530

    Class for Speaking Engine for the Assistant. Contains following methods: __init__ --> args(rate,voice) speak --> args(string)
    Description:
    SpeakerEngine initializes the 'engine' for speech capabilities for the assistant. It's defaults are
        * rate=150
        * voice=f5.\n
        There are other voices available in the host system such as
        * f1, f2, ...f5
        * mb-us1 (mbrola for less robotic and stiff sound)
        * m1, m2, ...m5
    
    No issues so far

---
+++ dependencies/SpeakerEngine.py
@@ -0,0 +1,32 @@
+import pyttsx3
+
+
+class SpeakerEngine:
+    '''
+    SpeakerEngine initializes the 'engine' for speech capabilities for the assistant. It's defaults are 
+    * rate=150
+    * voice=f5.\n
+    There are other voices available in the host system such as 
+    * f1, f2, ...f5
+    * mb-us1 (mbrola for less robotic and stiff sound)
+    * m1, m2, ...m5
+    '''
+
+    def __init__(self, rate: int = 150, voice: str = 'f5'):
+        try:
+            # initializing the speaking engine
+            self.engine = pyttsx3.init()
+
+            # setting voice for speech synthesizer
+            self.engine.setProperty('voice', voice)
+
+            # Speech rate set to 150
+            self.engine.setProperty('rate', rate)
+
+        except Exception as e:
+            print("Error while initializing the Speaker Engine!!!")
+            print(e)
+
+    def speak(self, string):
+        self.engine.say(string)
+        self.engine.runAndWait()
