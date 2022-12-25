4096e4c02e50342607f2c4cadb099dfd8f652dfd
waseemR02 <waseem.riaz.9999@gmail.com>
2022-12-26 00:14:09 +0530

    Tested the timeout feature of the Recognizer class.Pretty convenient and helpful.

--- testing/recognizer.py
+++ testing/recognizer.py
@@ -4,9 +4,9 @@ recording = sr.Recognizer()
 with sr.Microphone(device_index=0) as source: 
 	recording.adjust_for_ambient_noise(source)
 	print("Please Say something:")
-	audio = recording.listen(source)
+	audio = recording.listen(source, timeout=3)
 
 try:
-   print("You said: \n" + recording.recognize_google(audio))
+	print("You said: \n" + recording.recognize_google(audio))
 except Exception as e:
-   print(e)
+	print(e)
