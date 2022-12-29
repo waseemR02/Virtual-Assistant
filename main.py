695cd0f56c79d855894ffa7a998f69158d369896
waseemR02 <waseem.riaz.9999@gmail.com>
2022-12-30 00:37:46 +0530

    Update main.py for the new changes in features library

--- main.py
+++ main.py
@@ -1,17 +1,63 @@
 from dependencies.ListenerEngine import ListenerEngine
 from dependencies.SpeakerEngine import SpeakerEngine
+from features.greetings import Greetings
+from features.temporal import Temporal
+
+import keyboard
+import os
+
+
+class Assistant:
+    def __init__(self):
+        self.se = SpeakerEngine(rate=130, voice='f4')
+        self.le = ListenerEngine()
+
+        self.greetings = Greetings()
+        self.temporal = Temporal()
+
+        self.se.speak(self.greetings.wish())
+
+        while True:
+            if keyboard.is_pressed("space"):
+                self.aware()
+            else:
+                pass
+
+    def aware(self):
+        query = self.le.listen().lower()
+
+        if "about yourself" in query:
+            self.se.speak(self.greetings.aboutMe())
+            
+        elif ("what time is it now" in query) or ("what is the time now" in query) or ("what is the time" in query):
+            self.se.speak(self.temporal.timenow())
+            
+        elif "day is today" in query:
+            self.se.speak(self.temporal.daynow())
+        
+        elif (" is the month" in query) or ("month is it" in query):
+            self.se.speak(self.temporal.monthnow())
+        
+        elif (" is the year" in query) or (" year is it" in query):
+            self.se.speak(self.temporal.yearnow())
+        
+        elif ("is today's date" in query) or ("is the date today" in query):
+            self.se.speak(self.temporal.datenow())
+
+        elif "shut down yourself" in query:
+            try:
+                print("Terminating..")
+                os._exit()
+            except:
+                print("Error while while terminating!!")
+
+        else:
+            self.se.speak("I didn't quite understand you.")
+        print(query)
 
 
 def main():
-    se = SpeakerEngine(rate=150, voice='m2')
-    se.speak("Welcome back, sir")
-
-    le = ListenerEngine()
-    while True:
-        command = le.listen().lower()
-
-        print("you said: " + command)
-        se.speak("you said: " + command)
+    Assistant()
 
 
 if __name__ == "__main__":
