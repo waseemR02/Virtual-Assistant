703934e20292ff53c4d7bc61265c88dbba477c71
varshith-kali <pulivarshit@gmail.com>
2023-01-16 22:16:11 +0530

    Update method birthday to birthday_wish in greetings.py Randomised selection of quotes for returning expression Add dependency to random

--- features/greetings.py
+++ features/greetings.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random
 
 
 class Greetings:
@@ -25,19 +26,34 @@ class Greetings:
            and time or reporting weather forecast."""
         return expression
 
-    def birthday(self):
-        return "Many more happy returns of the day Sir. By the way I have a quote for you : " + rand(wishes);
-        ## Count your life by smiles, not tears. Count your age by friends, not years. Happy birthday!
-        ## A wish for you on your birthday, whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always. Happy birthday!
-        ## Happy birthday! Your life is just about to pick up speed and blast off into the stratosphere. Wear a seat belt and be sure to enjoy the journey. Happy birthday!
-        ## Count not the candles…see the lights they give. Count not the years, but the life you live. Wishing you a wonderful time ahead. Happy birthday.
-        ## Forget the past; look forward to the future, for the best things are yet to come.
-        ## Your birthday is the first day of another 365-day journey. Be the shining thread in the beautiful tapestry of the world to make this year the best ever. Enjoy the ride.”
-        ######################## Funny Quotes ##############################
-        ## You’re older today than yesterday but younger than tomorrow, happy birthday!
-        ## As you get older three things happen. The first is your memory goes, and I can’t remember the other two. Happy birthday!
-        ## Happy birthday! May your Instagram wall be filled with messages from people you never talk to.
-    
+    def birthday_wish(self):
+        """
+        This method returns an expression when the query is : Today is my birthday.
+
+        It returns an expression randomly chosen from a list.
+        """
+        quotes = [
+            "Count your life by smiles, not tears. Count your age by friends, not years. Happy birthday!",
+            """A wish for you on your birthday, whatever you ask may you receive, 
+            whatever you seek may you find, whatever you wish may it be fulfilled
+            on your birthday and always. Happy birthday!""",
+            """Happy birthday! Your life is just about to pick up speed and blast off into the stratosphere. 
+            Wear a seat belt and be sure to enjoy the journey. Happy birthday!""",
+            """Count not the candles…see the lights they give. Count not the years, but the life you live. 
+            Wishing you a wonderful time ahead. Happy birthday.""",
+            """Forget the past; look forward to the future, for the best things are yet to come.""",
+            """Your birthday is the first day of another 365-day journey. Be the shining thread in the beautiful
+             tapestry of the world to make this year the best ever. Enjoy the ride.""",
+            """You're older today than yesterday but younger than tomorrow, happy birthday!""",
+            """As you get older three things happen. The first is your memory goes, and I can't remember the other two. Happy birthday!""",
+            """Happy birthday! May your Instagram wall be filled with messages from people you never talk to."""
+        ]
+
+        base_expression = "Many Many Happy returns of the day to you. I have a quote for you. "
+        expression = random.choice(quotes)
+
+        return base_expression + expression
+
     def girlfriend(self):
         return "I am in relationship with Alexa since 4 years"
 
@@ -45,17 +61,13 @@ class Greetings:
         return "I was created on 4-January-2023 by professor Waseem"
 
     def enemy(self):
-        return "I donot have enemies. In fact I love them, friends ask you questions and enemies question you."
+        return "I do not have enemies. In fact I love them, friends ask you questions and enemies question you."
 
     def strength(self):
-        return 
+        return
 
     def weakness(self):
         return "The biigest weakness of mine is not having a physical presence. I donot have the feature that makes me communicate face to face."
 
     def inspiration(self):
         return "My inspiration is professor Joseph Weizenbaum. He was the first person to create a natural language processing computer program or chatbot known as ELIZA. It was created to demonstrate that the communication between man and machine was superficial."
-
-    
-
-
